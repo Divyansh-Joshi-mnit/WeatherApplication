734e52c552da029c69a558fea1c110ec47f293dc
Divyansh Joshi <div6982@gmail.com>
2025-06-22 15:41:20 +0530

    Added Javascript for handling the api req/res and displaying data

---
+++ index.js
@@ -0,0 +1,76 @@
+const apiKey ="39de5722b7922c943672a96862251900";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+
+const input = document.querySelector('input');
+// console.log(input);
+const search = document.querySelector("button");
+const weather_space = document.querySelector('.ErrorMessage');
+
+search.addEventListener("click", () => {
+    const city = input.value.trim();
+    console.log(`Entered city : ${city}`)
+    input.value = "";
+    checkWeather(city);
+
+});
+
+input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        const city = input.value.trim();
+        input.value = "";
+        checkWeather(city);
+
+    }
+});
+
+let img_to_show= document.querySelector('.weather-icon');
+let temp = document.querySelector('.temp');
+let ciity = document.querySelector('.city')
+let humidity = document.querySelector('.humidity');
+let wind = document.querySelector('.wind');
+
+
+
+const checkWeather=async (city)=>{
+    try{    
+
+            const response = await fetch(apiUrl + `&appid=${apiKey}` + `&q=${city}`);
+            if (!response.ok) throw new Error("City not found");
+            let data = await response.json();
+    
+            let temperature = data.main.temp;
+            let wind_speed = data.wind.speed;
+            let img = data.weather[0].main.toLowerCase();
+           
+            img_to_show.src = `./images/${img}.png`;
+            
+            wind.innerHTML = `${wind_speed} km/h`;
+            temp.innerHTML = `${temperature}°C `;
+
+
+            let city_ = data.name;
+            ciity.innerHTML = city_;
+            humidity.innerHTML = `${data.main.humidity} %`;
+            weather_space.innerHTML="";
+            document.querySelector('.weather').style.display = 'block';
+    }
+    catch(error){
+            // console.error(error);
+            weather_space.innerHTML ="Please Enter a valid city 😭";
+            document.querySelector('.weather').style.display = 'none';
+    }
+
+        }
+
+// data.main.temp : Temperature 
+// data.wind.speed : Wind-Speed 
+// data.weather[0].main : Clouds (Image to show)
+// data.name
+// data.main.humidity : humidity percentage 
+
+
+
+
+
+
+
